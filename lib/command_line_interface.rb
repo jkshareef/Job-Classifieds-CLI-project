3ed91601465f8d8db3bb93e9edad0941e97542d2
github username <github email address>
2019-05-07 16:26:02 -0700

    Created command line interface, updated run file with methods and changed enviornment file to remove the debuger when updating instance in database

---
+++ lib/command_line_interface.rb
@@ -0,0 +1,26 @@
+require_all './app/models'
+
+def welcome_message
+  puts "Hello job seeker! Welcome to SMARTER CLASSIFIEDS!
+
+In order for us to find the jobs best suited to you, we will ask you to quickly create a
+profile.
+
+In order, we are going to ask you to enter your name, your skill-set (please enter all of
+your skills separated by spaces), your experience in your current field (in years), and the
+location in which you are searching for a job. "
+end
+
+def gets_user_data
+
+  new_user = User.create()
+
+  print "Name: "
+  new_user.update(name: gets.chomp)
+  print "Skills: "
+  new_user.update(skills: gets.chomp)
+  print "Experience: "
+  new_user.update(experience: gets.chomp)
+  print "Location: "
+  new_user.update(location: gets.chomp)
+end

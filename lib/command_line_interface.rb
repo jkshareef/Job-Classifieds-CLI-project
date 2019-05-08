85fb35d07be3d1ef0bf36d56a63f448f5b9c96d5
github username <github email address>
2019-05-08 15:37:45 -0700

    migrated delete/view over from working file

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -13,9 +13,9 @@ def run
     elsif input == "2"
       search_manual_entry
       save_job_with_interest_rating
-    # elsif input == "3"
-    #   #view and edit jobs
-    # elsif input == "4"
+    elsif input == "3"
+      view_and_edit_jobs
+  # elsif input == "4"
     #   #update profile
     end
     menu
@@ -182,4 +182,27 @@ end
 
 def add_interest_rating(rating)
   SavedJob.last.update(interest_level: rating)
+end
+
+def view_and_edit_jobs
+  jobs = User.last.saved_jobs
+  jobs.each_with_index do |saved_job, index|
+    puts "#{index + 1}. #{saved_job.job.company}, id:#{saved_job.job.id}"
+    puts saved_job.job.description
+  end
+  puts "Would you like to delete jobs from your list? (y/n)"
+  input = gets.chomp
+  if input.downcase == "y"
+    puts "Would you like to delete all saved jobs? (y/n)"
+    puts "Enter (y, n, menu)"
+    input = gets.chomp
+    if input.downcase == "y"
+      User.last.saved_jobs.destroy_all
+    elsif input.downcase == "n"
+      puts "Which job would you like to delete?"
+      puts "Enter job id"
+      input = gets.chomp
+      Job.find(input).destroy
+    end
+  end
 end

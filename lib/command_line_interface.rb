641522f341bdccaf4c1ce9374c3db87f616076dd
github username <github email address>
2019-05-08 12:06:59 -0700

    created search_arbitrary_criteria in cli

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,7 +1,7 @@
 require_all './app/models'
 
 def welcome_message
-  puts "Hello job seeker! Welcome to SMARTER CLASSIFIDES!
+  puts "Hello job seeker! Welcome to SMARTER CLASSIFIEDS!
 
 In order for us to find the jobs best suited to you, we will ask you to quickly create a
 profile.
@@ -46,66 +46,59 @@ puts "     ______________
 `-----------------------'"
 end
 
- def auto_search
 
-  ### Searches Job Database and returns positions for any match to user location and either a keyword match form the users skills with either the job title or job description
-#
+
+def auto_search
+  keywords = User.last.skills.scan(/\w+/)
+
    results = Job.all.select { |job|
-     (skill_match_title?(job) || skill_match_description?(job)) && location_match?(job)
+     (skill_match_title?(keywords, job) || skill_match_description?(keywords, job)) && location_match?(job)
    }
-#
-#
-#   ### Uses the results of the search and
-#   #1. Creates a new SavedJob for each result
-#   #2. Associates the user to the SavedJob
-#   #3. Associates the job to the SavedJob
-#   #4. Adds the SavedJob to both User and Job
-#
-   results.each do |job|
-     print "Job #{job.id}."
-   #  print job.company
-     print job.title
-     print job.position_type
-     print job.description
-#
-#     # search_result = SavedJob.create
-#     # search_result.user = self
-#     # search_result.job = job_object
-#     # self.saved_jobs << search_result
-#     # job_object.saved_jobs << search_result
-  end
+
+   results.each { |job|
+     puts ' '
+     print '---------------------------------------------------------------------------------------------'
+     puts ' '
+     puts "Job #{job.id}."
+     puts ' '
+     puts "<<<#{job.company}>>>"
+     puts "--#{job.title}--"
+     puts "..#{job.position_type}.."
+     puts ' '
+     puts job.description
+   }
 end
-#
+
 def search_arbitrary_criteria
   print "Please enter keywords for your search separated by spaces: "
-  keywords = gets.chomp.split(' ')
+  keywords = gets.chomp.split(' ').flatten
 
   results = Job.all.select { |job|
-    (keywords.scan(/\w+/).any? {|word| job.title.include?(word)} || keywords.scan(/\w+/).any? {|word| job.description.include?(word)}) && location_match?(job)
+    (skill_match_title?(keywords, job) || skill_match_description?(keywords, job)) && location_match?(job)
   }
 
-  results.each do |job|
-    print "Job #{job.id}."
-  #  print job.company
-    print job.title
-    print job.position_type
-    print job.description
- end
+  results.each { |job|
+    puts ' '
+    print '---------------------------------------------------------------------------------------------'
+    puts ' '
+    puts "Job #{job.id}."
+    puts ' '
+    puts "<<<#{job.company}>>>"
+    puts "--#{job.title}--"
+    puts "..#{job.position_type}.."
+    puts ' '
+    puts job.description
+  }
 end
 
-
-
-def skill_match_title?(job)
-  ### Helper file, splits user skills string into words in an array and searches job title for any match to each individual word
-  User.last.skills.scan(/\w+/).any? {|word| job.title.include?(word)}
+def skill_match_title?(keywords, job)
+  keywords.any? {|word| job.title.include?(word)}
 end
 
-def skill_match_description?(job)
-    ### Helper file, splits user skills string into words in an array and searches job description for any match to each individual word
-  User.last.skills.scan(/\w+/).any? {|word| job.description.include?(word)}
+def skill_match_description?(keywords, job)
+  keywords.any? {|word| job.description.include?(word)}
 end
 
 def location_match?(job)
-    ### Helper file, compares location of user and of job
   job.location.downcase == User.last.location.downcase
 end

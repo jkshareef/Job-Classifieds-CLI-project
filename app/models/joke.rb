1204ed3eb9d15c70ad7f37e9d83b60f788a35f7d
github username <github email address>
2019-05-08 10:40:02 -0700

    moving searches to cli from user

--- app/models/joke.rb
+++ app/models/joke.rb
@@ -1,6 +1,5 @@
 class Joke < ActiveRecord::Base
 
-
   def self.get_choices
     response_string = RestClient.get("https://official-joke-api.appspot.com/jokes/programming/ten")
     response_hash = JSON.parse(response_string)
